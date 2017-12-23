4a06732f91121ad1883e9a87bd7aa85672c01c4b
Michael Riedmann <michael_riedmann@live.com>
2017-12-23 14:15:44 +0100

    Rewrite (#15)
    * complete rewrite
    
    * added docker-compose file
    
    * added .env support to docker-compose
    
    * removed asset and runtime volumes
    
    * added very basic migration feature
    
    * disabled interactive-mode on migration. fix #12
    
    * set ownership of folders after copy. fix #10
    
    * updated to humhub version 1.2.3

---
+++ docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "=="
+if [ -f "/var/www/localhost/htdocs/protected/config/.version" ]; then
+  echo "installation found"
+  INSTALL_VERSION=`cat /var/www/localhost/htdocs/protected/config/.version`
+  SOURCE_VERSION=`cat /usr/src/humhub/.version`
+  if [[ $INSTALL_VERSION != $SOURCE_VERSION ]]; then
+    echo "updating from version $INSTALL_VERSION to $SOURCE_VERSION"
+    cd /var/www/localhost/htdocs/protected/
+    php yii migrate/up --includeModuleMigrations=1 --interactive=0
+    cp -v /usr/src/humhub/.version /var/www/localhost/htdocs/protected/config/.version
+  fi
+else
+  echo "installing source files"
+  cp -rv /usr/src/humhub/uploads/* /var/www/localhost/htdocs/uploads/
+  cp -rv /usr/src/humhub/protected/config/* /var/www/localhost/htdocs/protected/config/
+  cp -v /usr/src/humhub/.version /var/www/localhost/htdocs/protected/config/.version
+
+  chown -R nginx:nginx /var/www/localhost/htdocs/uploads
+  chown -R nginx:nginx /var/www/localhost/htdocs/protected/modules
+  chown -R nginx:nginx /var/www/localhost/htdocs/protected/config
+fi
+echo "=="
+
+exec "$@"
