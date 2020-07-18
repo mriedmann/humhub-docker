415cc31bfe0cd25cf2928a594f42a2ab3def7fe0
Michael Riedmann <michael_riedmann@live.com>
2020-07-18 22:34:28 +0200

    fixed repo autoupdate

--- update.sh
+++ update.sh
@@ -4,9 +4,11 @@ set -eo pipefail
 
 CUR_VERSION=$(grep 'ARG HUMHUB_VERSION=' Dockerfile | tr "=" "\n" | tail -n 1)
 NEW_VERSION=${NEW_VERSION:-$(curl -s https://api.github.com/repos/humhub/humhub/releases | jq -r '.[0] | .name')}
+GIT_BRANCH="update-$NEW_VERSION"
 
 export CUR_VERSION
 export NEW_VERSION
+export GIT_BRANCH
 
 if [ "$CUR_VERSION" == "$NEW_VERSION" ]; then
     echo "no update needed"
@@ -15,7 +17,7 @@ else
     echo "updating from $CUR_VERSION to $NEW_VERSION"
 fi
 
-GIT_BRANCH="update-$NEW_VERSION"
+
 
 git branch "$GIT_BRANCH" || true
 git checkout "$GIT_BRANCH"
@@ -24,3 +26,6 @@ sed -i -e "s/ARG HUMHUB_VERSION=[0-9\.]*/ARG HUMHUB_VERSION=$NEW_VERSION/" Docke
 
 sed -i -e "s/humhub:$CUR_VERSION/humhub:$NEW_VERSION/g" README.md
 sed -i -e "s/\`$CUR_VERSION\`/\`$NEW_VERSION\`/g" README.md
+
+git add README.md Dockerfile
+git commit -m "update from $CUR_VERSION to $NEW_VERSION"
