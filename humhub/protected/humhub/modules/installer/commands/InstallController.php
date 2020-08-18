a2ca3d526911facacd0b66c2b74f3a14f1c8e9f4
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-08-18 18:45:21 +0200

    :bug: Fix missing password param
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- humhub/protected/humhub/modules/installer/commands/InstallController.php
+++ humhub/protected/humhub/modules/installer/commands/InstallController.php
@@ -176,7 +176,7 @@ class InstallController extends Controller
     /**
      * Creates a new user account.
      */
-    private function createUser($username, $email, $pass, $title, $firstname, $lastname): User
+    private function createUser(string $username, string $email, string $pass, string $title, string $firstname, string $lastname): User
     {
         $user = new User();
         $user->username = $username;
@@ -201,7 +201,7 @@ class InstallController extends Controller
     /**
      * Sets the password for a user account
      */
-    private function setUserPassword(User $user)
+    private function setUserPassword(User $user, string $pass)
     {
         $password = new Password();
         $password->user_id = $user->id;
