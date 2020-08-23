9cbe4f0ff42664750ef7b29728572d4690306165
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-08-23 22:48:30 +0200

    :bug: Fix sed replace
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -93,7 +93,7 @@ if test -e /var/www/localhost/htdocs/protected/config/dynamic.php &&
 
 	if [ "$HUMHUB_TRUSTED_HOSTS" != "false" ]; then
 		sed -i \
-			-e "s/'trustedHosts' => \['127.0.0.1/32'\]/'trustedHosts' => ['$HUMHUB_TRUSTED_HOSTS']/g" \
+			-e "s|'trustedHosts' => \['.*'\]|'trustedHosts' => ['$HUMHUB_TRUSTED_HOSTS']|g" \
 			/var/www/localhost/htdocs/protected/config/web.php
 	fi
 else
