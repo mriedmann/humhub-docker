7061ce72c5e73061322729a222779c92925c8631
Michael Riedmann <michael_riedmann@live.com>
2021-03-10 19:02:18 +0100

    refactor: :sparkles: revived update.sh

--- update.sh
+++ update.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-set -eo pipefail
+set -xeo pipefail
 
-CUR_VERSION=$(grep 'ARG HUMHUB_VERSION=' Dockerfile | tr "=" "\n" | tail -n 1)
-NEW_VERSION=${NEW_VERSION:-$(curl -s https://api.github.com/repos/humhub/humhub/releases | jq -r '.[0] | .name')}
-GIT_BRANCH="update-$NEW_VERSION"
+UPDATE_NEEDED=false
+CUR_VERSION=""
+NEW_VERSION=""
+GIT_BRANCH=""
 
-export CUR_VERSION
-export NEW_VERSION
-export GIT_BRANCH
+upstream_versions=$(curl -s https://api.github.com/repos/humhub/humhub/releases | jq -r '.[] | select(.prerelease==false) | .name' | sort -n)
+local_versions=$(cat versions.txt) # to avoid problems when writing doing loop
+while IFS= read -r line; do
+    local_version_prefix=$(echo "$line" | cut -d' ' -f2)
+    local_version=$(echo "$line" | cut -d' ' -f1)
+    latest_upstream_version=$(echo "$upstream_versions" | grep "$local_version_prefix" | tail -n1)
+    if [ "$local_version" != "$latest_upstream_version" ]; then
+        echo "$local_version_prefix: UPDATE NEEDED! ($local_version != $latest_upstream_version)"
+        postfix=${line/$local_version $local_version_prefix/}
+        sed -i "s/$line/$latest_upstream_version $local_version_prefix$postfix/" versions.txt
+        CUR_VERSION="$local_version"
+        NEW_VERSION="$latest_upstream_version"
+        UPDATE_NEEDED=true
+        export CUR_VERSION
+        export NEW_VERSION
+        export UPDATE_NEEDED
+        break
+    else
+        echo "$local_version_prefix: no update needed ($local_version == $latest_upstream_version)"
+    fi
+done <<< "$local_versions"
 
-if [ "$CUR_VERSION" == "$NEW_VERSION" ]; then
-    echo "no update needed"
-    exit 1
-else
-    echo "updating from $CUR_VERSION to $NEW_VERSION"
+if [ $UPDATE_NEEDED ]; then
+    GIT_BRANCH="update-$NEW_VERSION"
+    export GIT_BRANCH
+
+    git branch "$GIT_BRANCH" || true
+    git checkout "$GIT_BRANCH"
+
+    git add versions.txt
+    git commit -m "update from $CUR_VERSION to $NEW_VERSION"
 fi
-
-
-
-git branch "$GIT_BRANCH" || true
-git checkout "$GIT_BRANCH"
-
-sed -i -e "s/ARG HUMHUB_VERSION=[0-9\.]*/ARG HUMHUB_VERSION=$NEW_VERSION/" Dockerfile
-
-sed -i -e "s/humhub:$CUR_VERSION/humhub:$NEW_VERSION/g" README.md
-sed -i -e "s/\`$CUR_VERSION\`/\`$NEW_VERSION\`/g" README.md
-
-git add README.md Dockerfile
-git commit -m "update from $CUR_VERSION to $NEW_VERSION"
