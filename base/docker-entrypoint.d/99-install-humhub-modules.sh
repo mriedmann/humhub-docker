9f4bd7868ee301b4541a3b6125d74287bdb46d5a
dantefromhell <90508808+dantefromhell@users.noreply.github.com>
2022-02-20 22:44:43 +0000

    Disable bash debug output

--- base/docker-entrypoint.d/99-install-humhub-modules.sh
+++ base/docker-entrypoint.d/99-install-humhub-modules.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# TODO: Remove after this script has matured more more.
-set -x
-
 # Space seperated list of modules names to install, as displayed in the
 # 'ID' column of `php yii module/list-online` command.
 HUMHUB_MODULES_ENABLED="calendar cfiles content-bookmarks gallery newmembers polls scrollup spotify tasks wiki"
