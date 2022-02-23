3421adba97d0fa5bb62bafe78fabbe1f499bebec
dantefromhell <90508808+dantefromhell@users.noreply.github.com>
2022-02-23 19:42:59 +0000

    Add HUMHUB_{DISABLE,ENABLE}_VARIABLE to allow docker env variables

--- base/docker-entrypoint.d/99-install-humhub-modules.sh
+++ base/docker-entrypoint.d/99-install-humhub-modules.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-# Space seperated list of modules names to install, as displayed in the
+# Space seperated list of modules enabled per default, as displayed in the
 # 'ID' column of `php yii module/list-online` command.
-HUMHUB_MODULES_ENABLED="calendar cfiles content-bookmarks external_calendar gallery newmembers polls scrollup spotify tasks wiki"
+ENABLED_MODULES="calendar cfiles content-bookmarks external_calendar gallery newmembers polls scrollup spotify tasks wiki"
+
+echo "Modules enabled by docker env: ${HUMHUB_ENABLE_MODULES}"
+echo "Modules disabled by docker env: ${HUMHUB_DISABLE_MODULES}"
+
 
 echo "Installing HumHub modules"
-for module in ${HUMHUB_MODULES_ENABLED}
+for module in ${ENABLED_MODULES}
 do
     echo "... Installing module ${module}"
     su -s /bin/sh nginx -c "php yii module/install ${module}"
@@ -16,12 +20,12 @@ done
 echo "Module installation finished."
 
 
-# Space separated list of modules name to disable, as displayed in the
+# Space separated list of modules name disabled per default, as displayed in the
 # 'ID' column of `php yii module/list-online` command.
-HUMHUB_MODULES_DISABLED=''
+DISABLED_MODULES=''
 
 echo "Disabling HumHub modules"
-for module in ${HUMHUB_MODULES_DISABLED}
+for module in ${DISABLED_MODULES}
 do
     echo "... Disabling module ${module}"
     su -s /bin/sh nginx -c "php yii module/disable ${module}"
