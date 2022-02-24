9534ed2437ac5aaba257623ade1df3ab3a4e7956
dantefromhell <90508808+dantefromhell@users.noreply.github.com>
2022-02-24 17:35:35 +0000

    Add debug output to integrity check

--- base/docker-entrypoint.d/50-humhub-validate.sh
+++ base/docker-entrypoint.d/50-humhub-validate.sh
@@ -2,6 +2,11 @@
 
 HUMHUB_INTEGRITY_CHECK=${HUMHUB_INTEGRITY_CHECK:-1}
 
+# TODO: Remove when debugging successful finished
+echo >&3 "$0: Cache folder before integrity check"
+date
+ls -laR /var/www/localhost/htdocs/protected/runtime
+
 if [ "$HUMHUB_INTEGRITY_CHECK" != "false" ]; then
 	echo "validating ..."
 	if ! php ./yii integrity/run; then
@@ -11,3 +16,8 @@ if [ "$HUMHUB_INTEGRITY_CHECK" != "false" ]; then
 else
 	echo "validation skipped"
 fi
+
+# TODO: Remove when debugging successful finished
+echo >&3 "$0: Cache folder after integrity check"
+date
+ls -laR /var/www/localhost/htdocs/protected/runtime
