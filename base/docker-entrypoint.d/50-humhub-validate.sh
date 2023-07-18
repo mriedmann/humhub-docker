dbdb93bac218f25e3e4d02259870954d367f0067
Michael Riedmann <michael_riedmann@live.com>
2023-07-18 13:13:28 +0200

    Update 50-humhub-validate.sh
    disable interactive on integrity check (fix #149)

--- base/docker-entrypoint.d/50-humhub-validate.sh
+++ base/docker-entrypoint.d/50-humhub-validate.sh
@@ -4,7 +4,7 @@ HUMHUB_INTEGRITY_CHECK=${HUMHUB_INTEGRITY_CHECK:-1}
 
 if [ "$HUMHUB_INTEGRITY_CHECK" != "false" ]; then
 	echo "validating ..."
-	if ! php ./yii integrity/run; then
+	if ! php ./yii integrity/run --interactive 0; then
 		echo "validation failed!"
 		exit 1
 	fi
