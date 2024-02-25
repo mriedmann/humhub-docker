589c2a71fde055dcfc954c82aab6cb60fd2cf256
dantefromhell <90508808+dantefromhell@users.noreply.github.com>
2024-02-25 03:08:21 +0000

    Simplify HumHub configuration
    Instead of separate file that contains config parameters applied via `yii setting/set` this mechanism uses the previously introduced ConfigController in combination with the (undocumented) override mechanism for the `SettingsManager`.
    
    See https://community.humhub.com/content/perma?id=275570 for more details

--- base/docker-entrypoint.d/95-configure-humhub.sh
+++ base/docker-entrypoint.d/95-configure-humhub.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# This script reads configuration options for a file provided via environment variable
-# HUMHUB_CONFIG_FILE and applies the settings sequentially to this container during startup.
+# This script executes a ConfigController that parses all PHP files in @app/config/common.d/*.php
+# into @app/config/common.php
 
 # Stop script execution on weird shell conditions.
 set -o errexit -o nounset
@@ -12,26 +12,5 @@ if [ "${TRACE:-0}" = "1" ]; then
 fi
 
 echo "START === Configuring HumHub options"
-CONFIG_FILE="${HUMHUB_CONFIG_FILE:-none}"
-
-# Stop if nothing needs to be done.
-if [ "${CONFIG_FILE}" = "none" ]; then
-  echo "No config file (or name none) provided, aborting..."
-  exit 0
-fi
-
-# Check the file actually exists and is readable and has content.
-if [ ! -r "${CONFIG_FILE}" ]; then
-  echo "Config file ${CONFIG_FILE} either does not exist or is not readable."
-fi
-if [ ! -s "${CONFIG_FILE}" ]; then
-  echo "Config file ${CONFIG_FILE} seems to be empty."
-fi
-
-# Actually do the stuff.
-grep -vE '^(\s*$|#)' "${CONFIG_FILE}" | while read -r LINE; do
-  echo "Found config line: ${LINE}"
-  su -s /bin/sh nginx -c "php /var/www/localhost/htdocs/protected/yii settings/set ${LINE} --interactive=0"
-done
-
+su -s /bin/sh nginx -c "php /var/www/localhost/htdocs/protected/yii humhub/cfg-gen-common --interactive=0"
 echo "END === DONE configuring HumHub"
