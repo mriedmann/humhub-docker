16ef6b45593d05c327df591aba60ca9e4e2d7f50
Jérémy Viès <4450519+jvies@users.noreply.github.com>
2026-03-02 18:00:47 +0100

    update.sh: fix for new versions.txt format

--- update.sh
+++ update.sh
@@ -7,27 +7,29 @@ CUR_VERSION=""
 NEW_VERSION=""
 GIT_BRANCH=""
 
-upstream_versions=$(curl -s https://api.github.com/repos/humhub/humhub/releases | jq -r '.[] | select(.prerelease==false) | .name' | sort -n)
-local_versions=$(cat versions.txt) # to avoid problems when writing doing loop
-while IFS= read -r line; do
-    local_version_prefix=$(echo "$line" | cut -d' ' -f2)
-    local_version=$(echo "$line" | cut -d' ' -f1)
-    latest_upstream_version=$(echo "$upstream_versions" | grep "$local_version_prefix" | tail -n1)
-    if [ "$local_version" != "$latest_upstream_version" ]; then
-        echo "$local_version_prefix: UPDATE NEEDED! ($local_version != $latest_upstream_version)"
-        postfix=${line/$local_version $local_version_prefix/}
-        sed -i "s/$line/$latest_upstream_version $local_version_prefix$postfix/" versions.txt
-        CUR_VERSION="$local_version"
-        NEW_VERSION="$latest_upstream_version"
-        UPDATE_NEEDED=true
-        export CUR_VERSION
-        export NEW_VERSION
-        export UPDATE_NEEDED
-        break
-    else
-        echo "$local_version_prefix: no update needed ($local_version == $latest_upstream_version)"
-    fi
-done <<< "$local_versions"
+upstream_versions=$(curl -s https://api.github.com/repos/humhub/humhub/releases | jq -r '.[] | select(.prerelease==false) | .name' | sort --version-sort)
+
+HUMHUB_VERSION=$(cat versions.txt)
+MAJOR_VERSION=$(echo "$HUMHUB_VERSION" | cut -d'.' -f1)
+MINOR_VERSION=$(echo "$HUMHUB_VERSION" | cut -d'.' -f2)
+HUMHUB_MACRO_VERSION=${MAJOR_VERSION}.${MINOR_VERSION}
+
+local_version=$HUMHUB_VERSION
+local_version_prefix=$HUMHUB_MACRO_VERSION
+
+latest_upstream_version=$(echo "$upstream_versions" | grep "$local_version_prefix" | tail -n1)
+if [ "$local_version" != "$latest_upstream_version" ]; then
+    echo "$local_version_prefix: UPDATE NEEDED! ($local_version != $latest_upstream_version)"
+    echo "$latest_upstream_version" > versions.txt
+    CUR_VERSION="$local_version"
+    NEW_VERSION="$latest_upstream_version"
+    UPDATE_NEEDED=true
+    export CUR_VERSION
+    export NEW_VERSION
+    export UPDATE_NEEDED
+else
+    echo "$local_version_prefix: no update needed ($local_version == $latest_upstream_version)"
+fi
 
 if [ $UPDATE_NEEDED == true ]; then
     GIT_BRANCH="update-$NEW_VERSION"
